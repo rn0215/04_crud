e7f0c37c8afd55eda24794aeb9f3087618ea9a5d
jongmin <rn021548@naver.com>
2019-02-08 04:58:25 +0000

    4번째 프로젝트

---
+++ movie/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index),
+    path('<int:id>/',views.read),
+    path('new/',views.new),
+    path('create/',views.create),
+    path('<int:id>/delete/',views.delete),
+    path('<int:id>/edit/',views.edit),
+    path('<int:id>/update/',views.update),
+]
